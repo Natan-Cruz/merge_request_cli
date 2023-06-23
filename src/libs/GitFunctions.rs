5a72f7d925ee0e4fbdb3907c092dc20f7f3e887d
Natã Alves <nata.cruz@multiplier.com.br>
2023-06-23 11:57:55 -0300

    adicionado diretorio de trabalho (pwd) ao projeto

--- src/libs/GitFunctions.rs
+++ src/libs/GitFunctions.rs
@@ -3,11 +3,19 @@ use std::{
         Output, 
         Command
     }, 
-    string::FromUtf8Error
+    string::FromUtf8Error,
+    env::current_dir
 };
 
 pub fn get_all_branchs() -> String {
-    let all_branchs: Output = Command::new("git").args(["branch", "-a"]).output().unwrap();
+    let curr_dir = current_dir().unwrap();
+
+    let curr_dir: &str = match curr_dir.to_str() {
+        Some(directory) => directory,
+        None => panic!("Não foi possível ler o work directory")
+    };
+
+    let all_branchs: Output = Command::new("git").args(["-C", curr_dir, "branch", "-a"]).output().unwrap();
     let all_branchs: Result<String, FromUtf8Error> = String::from_utf8(all_branchs.stdout);
     match all_branchs {
         Ok(all_branchs) => return all_branchs,
@@ -16,7 +24,14 @@ pub fn get_all_branchs() -> String {
 }
 
 pub fn get_current_branch() -> String {
-    let git_current_branch: Output = Command::new("git").args(["rev-parse", "--abbrev-ref", "HEAD"]).output().unwrap();
+    let curr_dir = current_dir().unwrap();
+
+    let curr_dir: &str = match curr_dir.to_str() {
+        Some(directory) => directory,
+        None => panic!("Não foi possível ler o work directory")
+    };
+
+    let git_current_branch: Output = Command::new("git").args(["-C", curr_dir, "rev-parse", "--abbrev-ref", "HEAD"]).output().unwrap();
 
     let git_current_branch: Result<String, FromUtf8Error> = String::from_utf8(git_current_branch.stdout);
     
