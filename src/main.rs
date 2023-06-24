d6cab6d62c87aaf11b3a63a9e562be4762a99353
Natã Alves <nata.cruz@multiplier.com.br>
2023-06-24 20:20:54 -0300

    adicionado na funcao de mr tentativas

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ use crate::questionnaire::{
     QuestionnaireMain
 };
 
-use json::JsonValue;
+use json::{JsonValue, object};
 use loading::{Loading, Spinner};
 
 
@@ -30,77 +30,81 @@ async fn main() -> ExitCode {
     let mut authorization: Authorization = Authorization::new();
     let configurations: Configurations = Configurations::new();
 
-    if authorization.api_token.is_empty() {
-        let initial_config = QuestionnaireInitial::Questionnaire::start_questionnaire().await;
-        authorization = Authorization::save_values(&initial_config.api_token, &initial_config.profile_id)
-    }
+    // if authorization.api_token.is_empty() {
+    //     let initial_config = QuestionnaireInitial::Questionnaire::start_questionnaire().await;
+    //     authorization = Authorization::save_values(&initial_config.api_token, &initial_config.profile_id)
+    // }
 
-    if configurations.scopes.is_empty() {
-        panic!("Escopo do projeto não estar vazio, vá até o Config.toml e preecha o campo scopes")
-    }
+    // if configurations.scopes.is_empty() {
+    //     panic!("Escopo do projeto não estar vazio, vá até o Config.toml e preecha o campo scopes")
+    // }
 
-    let answers = QuestionnaireMain::Questionnaire::start_questionnaire(
-        &authorization.api_token, 
-        &configurations.project_id, 
-        configurations.scopes, 
-        configurations.prefix, 
-        configurations.default_target_branch,
-    ).await;
+    // let answers = QuestionnaireMain::Questionnaire::start_questionnaire(
+    //     &authorization.api_token, 
+    //     &configurations.project_id, 
+    //     configurations.scopes, 
+    //     configurations.prefix, 
+    //     configurations.default_target_branch,
+    // ).await;
 
-    let merge_request_body:JsonValue = Utils::build_merge_request_body(
-        &answers, 
-        authorization.profile_id, 
-        configurations.reviewers,
-        configurations.repository
-    );
+    // let merge_request_body:JsonValue = Utils::build_merge_request_body(
+    //     &answers, 
+    //     authorization.profile_id, 
+    //     configurations.reviewers,
+    //     configurations.repository
+    // );
 
-    let loading_merge_request = Loading::new(Spinner::new(vec!["...", "●..", ".●.", "..●"]));
-    loading_merge_request.text("Criando Merge Request");
+    let merge_request_body = object!{
+        "repository": "teste" ,
+        "sourceBranch": "teste" ,
+        "targetBranch": "teste" ,
+        "title": "teste" ,
+        "description": "teste" ,
+        "reviewers": "teste" 
+    };
 
-    let merge_request_number = MergeRequest::create_merge_request(&authorization.api_token, merge_request_body, &configurations.project_id).await;
+
+    let merge_request_number: Result<i16, String> = MergeRequest::create_merge_request(&authorization.api_token, merge_request_body, &configurations.project_id).await;
 
     match merge_request_number {
         Ok(number) => {
-            let success_message = format!("Link para MR: https://multiplier.jetbrains.space/p/srp/reviews/{}/timeline", number);
-            loading_merge_request.success(success_message);
-            loading_merge_request.end();
+            println!("Link para MR: https://multiplier.jetbrains.space/p/srp/reviews/{}/timeline", number);
         },
         Err(error_message) => {
-            loading_merge_request.fail(error_message);
-            loading_merge_request.end();
+            println!("{:?}", error_message);
             return ExitCode::FAILURE
         } 
     }
     
-    let concatenated_number_issues: String = answers.issues.clone();
-    let number_issues: Vec<&str> = concatenated_number_issues
-        .split(" ")
-        .filter(| issue | !issue.is_empty())
-        .collect::<Vec<&str>>();
+    // let concatenated_number_issues: String = answers.issues.clone();
+    // let number_issues: Vec<&str> = concatenated_number_issues
+    //     .split(" ")
+    //     .filter(| issue | !issue.is_empty())
+    //     .collect::<Vec<&str>>();
 
     
-    if !number_issues.is_empty(){
-        for number_issue in number_issues {
+    // if !number_issues.is_empty(){
+    //     for number_issue in number_issues {
 
-            let loading_update_status_issue = Loading::new(Spinner::new(vec!["...", "●..", ".●.", "..●"]));
+    //         let loading_update_status_issue = Loading::new(Spinner::new(vec!["...", "●..", ".●.", "..●"]));
         
-            loading_update_status_issue.text(format!("Alterando status da issue: {number_issue:?}"));
+    //         loading_update_status_issue.text(format!("Alterando status da issue: {number_issue:?}"));
 
-            let result: Result<String, String> = Issues::update_status(&authorization.api_token, &configurations.project_id, &number_issue).await;
+    //         let result: Result<String, String> = Issues::update_status(&authorization.api_token, &configurations.project_id, &number_issue).await;
 
-            match result {
-                Ok(_) => {
-                    loading_update_status_issue.text(format!("Status da Issue: {number_issue:?} alterado com sucesso"));
-                    loading_update_status_issue.end()
-                },
-                Err(error_message) => {
-                    loading_update_status_issue.fail(error_message);
-                    loading_update_status_issue.end();
-                    return ExitCode::FAILURE;
-                },
-            }
-        }
-    }
+    //         match result {
+    //             Ok(_) => {
+    //                 loading_update_status_issue.text(format!("Status da Issue: {number_issue:?} alterado com sucesso"));
+    //                 loading_update_status_issue.end()
+    //             },
+    //             Err(error_message) => {
+    //                 loading_update_status_issue.fail(error_message);
+    //                 loading_update_status_issue.end();
+    //                 return ExitCode::FAILURE;
+    //             },
+    //         }
+    //     }
+    // }
 
     ExitCode::SUCCESS
 }
--- src/requests/MergeRequest.rs
+++ src/requests/MergeRequest.rs
@@ -1,3 +1,5 @@
+use std::{thread, time};
+use loading::{Loading, Spinner};
 use reqwest::header::{CONTENT_TYPE, ACCEPT};
 use serde::{Serialize, Deserialize};
 use json::{JsonValue};
@@ -5,15 +7,17 @@ use json::{JsonValue};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Response {
-    number: u16
+    number: i16
 }
 
-pub async fn create_merge_request(api_token: &str, params: JsonValue, project_id: &String) -> Result<String, String> {
+async fn _create_merge_request(api_token: &str, params: JsonValue, project_id: &String) -> Result<i16, String> {
     let mut url = String::new();
 
-    url.push_str("https://multiplier.jetbrains.space/api/http/projects/id:");
-    url.push_str(&project_id); 
-    url.push_str("/code-reviews/merge-requests?$fields=number"); 
+    // url.push_str("https://multiplier.jetbrains.space/api/http/projects/id:");
+    // url.push_str(&project_id); 
+    // url.push_str("/code-reviews/merge-requests?$fields=number"); 
+
+    url.push_str("http://localhost:3000");
 
     let client: reqwest::Client = reqwest::Client::new();
 
@@ -30,7 +34,7 @@ pub async fn create_merge_request(api_token: &str, params: JsonValue, project_id
         reqwest::StatusCode::OK => {
             // on success, parse our JSON to an APIResponse
             return match response.json::<Response>().await {
-                Ok(parsed) => Ok(parsed.number.to_string()),
+                Ok(parsed) => Ok(parsed.number),
                 Err(_) => Err("Erro ao tentar criar MR".to_string()),
             };
         }
@@ -41,4 +45,51 @@ pub async fn create_merge_request(api_token: &str, params: JsonValue, project_id
             return Err(format!("Algo de errado aconteceu: Http Status Code: {other:?}"));
         }
      };
+}
+
+pub async fn create_merge_request(api_token: &str, params: JsonValue, project_id: &String) -> Result<i16, String> {
+    let retry_attempts = 3;
+
+    let mut attempts = 0;
+    let mut merge_request_number = 0;
+
+    while attempts < retry_attempts {
+
+        // loading style
+        let loading = Loading::new(Spinner::new(vec!["...", "●..", ".●.", "..●"]));
+        loading.text("Criando Merge Request...");
+
+        // realiza criação da MR
+        let result = _create_merge_request(api_token, params.clone(), project_id).await;
+
+        match result {
+            Ok(number) => {
+                loading.success("Merge Request criado com sucesso");
+                merge_request_number = number
+            },
+            Err(error_message) => {
+                loading.fail(error_message);
+            } 
+        }
+
+        loading.end();
+
+        if merge_request_number != 0 {
+            break;
+        } else if attempts < 2 {
+            println!("Tentando novamente em alguns segundos...");
+        
+            // pausa por 3 segundos antes de tentar novamente
+            let three_seconds = time::Duration::from_secs(3);
+            thread::sleep(three_seconds);
+        }
+
+        attempts += 1;
+    }
+
+    if merge_request_number == 0 {
+        return Err("Não foi possível criar um MR, verifique sua conexão".to_string());
+    }
+
+    return Ok(merge_request_number)
 }
